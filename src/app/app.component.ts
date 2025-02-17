bb5887dc473db837c28e15578ccba218af1d273e
Marcel Angelo Navarro <marcelxangelo@gmail.com>
2025-02-17 11:50:42 +0800

    Template Driven Form Submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'template-driven';
+  title = 'template-form-app';
+  anytext: string = "";
+  firstName: string = "";
+  lastName: string = "";
+  email: string = "";
+  password: string = "";
+  phone: string = "";
+  handicap: number = 0;
+  teamName: string = "";
+  submitted: boolean = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
 }
